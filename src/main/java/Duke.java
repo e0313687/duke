e8b38b5cd5871161a9c624968c23179a2d54575c
e0313687 <e0313687@u.nus.edu>
2019-08-26 10:12:08 +0800

    Level 3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Duke {
     public static void main(String[] args) {
+
         //Level 0
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -24,7 +26,7 @@ public class Duke {
         }*/
 
         //Level 2
-        System.out.println("Hello! I'm Duke");
+        /*System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         String userCommands[] = new String[100];
         int temp = 0;
@@ -46,7 +48,40 @@ public class Duke {
                 temp++;
                 System.out.println("added: " + userCommand);
             }
-        }
+        }*/
 
+        //Level 3
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        Task[] userCommands = new Task[100];
+        int temp = 0;
+
+        while(true) {
+            Scanner command = new Scanner(System.in);
+            String userCommand = command.nextLine();
+            StringTokenizer defaultTokenizer = new StringTokenizer(userCommand);
+
+            if (userCommand.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            if (defaultTokenizer.countTokens() == 2 && defaultTokenizer.nextToken().equals("done")) {
+                int done = Integer.parseInt(defaultTokenizer.nextToken());
+                userCommands[done-1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + userCommands[done-1].getStatusIcon() + "] " + userCommands[done-1].getDescription());
+
+            }
+            else if (userCommand.equals("list")) {
+                for (int i = 0; i < temp; i++) {
+                    System.out.println(i+1 + ".[" + userCommands[i].getStatusIcon() + "] " + userCommands[i].getDescription());
+                }
+            }
+            else {
+                userCommands[temp] = new Task(userCommand);
+                temp++;
+                System.out.println("added: " + userCommand);
+            }
+        }
     }
 }
